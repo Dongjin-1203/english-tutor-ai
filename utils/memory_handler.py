e069e1439416c6d730071db3740294d57f578edb
dong1203 <hambur1203@gmail.com>
2025-10-22 22:15:06 +0900

    langchain memory bug fix2

--- utils/memory_handler.py
+++ utils/memory_handler.py
@@ -1,6 +1,7 @@
 # 대화 기록 저장
 # from langchain.memory import ConversationBufferMemory
 from langchain_core.memory import ConversationBufferMemory
+from langchain_community.memory import ConversationBufferMemory
 # 새 메모리 생성
 def create_memory():
     memory = ConversationBufferMemory(
